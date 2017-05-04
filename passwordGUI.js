a58fdaaad5b80595b3a214f88948de463289e6f2
SIGNN OUT OF YOUR GITHUB <7plutkovsky@stpatrick.org>
2017-05-04 13:44:21 -0500

    Add files via upload

---
+++ passwordGUI.js
@@ -0,0 +1,43 @@
+const readline = require('readline');
+const testString  =require('./string');
+
+
+function getString(){
+    let password = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    password.question("What would you like to set as your password?", function(str){
+        let passesAllChecks = true;
+
+        //Conditional Statements here.
+        if(testString.checkLength(str) == false){
+          passesAllChecks = false;
+        }
+        if(testString.containsUpper(str) == false){
+          passesAllChecks = false;
+        }
+        if(testString.containsLower(str) == false){
+          passesAllChecks = false;
+        }
+        if(testString.containsNumerical(str) == false){
+          passesAllChecks = false;
+        }
+        if(testString.containsSpecial(str) == false){
+          passesAllChecks = false;
+        }
+
+        if(!passesAllChecks){
+            console.log("\nTry again\n");
+            password.close();
+            getString();
+        }
+        else{
+            console.log("\nGreat password. Bye bye now...\n");
+            password.close();
+        }
+    });
+}
+
+getString();
